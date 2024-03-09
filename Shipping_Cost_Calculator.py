08635484b08db678429cbe4229b3aea90e11601b
masha1025 <94424703+masha1025@users.noreply.github.com>
2024-03-09 12:49:01 -0600

    Update Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,4 +11,9 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
  # Here is a new update by masha1025
+git config --global user.email "your-email@example.com"
+git config --global user.name "YourName"
 
+git add .
+git commit -m "added a new line to Shipping_Cost_Calculator.py"
+git status
